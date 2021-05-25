919359a76b75680041ccc56875118501ad188972
unknown <s6263269@gmail.com>
2021-05-26 04:19:12 +0800

    Add Flexible Header

---
+++ app.js
@@ -0,0 +1,23 @@
+let header = document.querySelector("header");
+let headerAnchor = document.querySelectorAll("header nav ul li a");
+
+
+
+window.addEventListener("scroll",() =>{
+    if(window.pageYOffset != 0){
+        header.style.backgroundColor = "rgba(0,0,0,0.8)";
+        header.style.color = "white";
+        headerAnchor.forEach( a => {
+            a.style.color ="white";
+        })
+
+
+    } else{
+        header.style = "";
+        header.style.color = "black";
+        headerAnchor.forEach( a => {
+            a.style.color ="#09777D";
+        })
+    }
+
+})
